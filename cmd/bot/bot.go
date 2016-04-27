88cff7804444491eae8d3917131ac3f288dc5fb0
Oxxygen <WatkinsC93@gmail.com>
2016-04-27 19:06:50 -0400

    Changing online status again, because I didnt like the last one.

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -438,7 +438,7 @@ func playSound(play *Play, vc *discordgo.VoiceConnection) (err error) {
 
 func onReady(s *discordgo.Session, event *discordgo.Ready) {
 	log.Info("Recieved READY payload")
-	s.UpdateStatus(0, "*Creative Status Here*")
+	s.UpdateStatus(0, "Dank Memes")
 }
 
 func onGuildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
